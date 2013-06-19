e67196fc32300145ad8e153ea7cac52bd8b7d4cf
killerwilmer@gmail.com <>
2013-06-19 09:59:29 -0500

    funcion y vista editar del fabricante.

--- default/app/controllers/fabricante_controller.php
+++ default/app/controllers/fabricante_controller.php
@@ -10,6 +10,12 @@
  *
  * @author hidden
  */
+
+/**
+ * Carga del modelo Menus...
+ */
+Load::models('fabricante');
+
 class FabricanteController extends ScaffoldController {
 
     //put your code here
@@ -27,10 +33,34 @@ class FabricanteController extends ScaffoldController {
 
                 $bd->exec("INSERT INTO fabricante(nombre, activo,fec) VALUES('" . $nombre . "', " . $activo . ", '" . $fecha . "')");
                 Flash::valid('Fabricante registrado exitosamente...!!!');
-            }
-            else{
+            } else {
                 Flash::error('Falta digitar el nombre del fabricante...!!!');
             }
+        }
+    }
+
+    /**
+     * Edita un Registro
+     *
+     * @param int $id (requerido)
+     */
+    public function editar($id) {
+        $fabricante = new Fabricante();
+
+        //se verifica si se ha enviado el formulario (submit)
+        if (Input::hasPost('fabricantes')) {
+            
+            $fabricante->fec = Timestamp::getTimeStamp();
+            if ($fabricante->update(Input::post('fabricantes'))) {
+                Flash::valid('Operación exitosa');
+                //enrutando por defecto al index del controller
+                return Router::redirect();
+            } else {
+                Flash::error('Falló Operación');
+            }
+        } else {
+            //Aplicando la autocarga de objeto, para comenzar la edición
+            $this->fabricantes = $fabricante->find_by_id((int) $id);
         }
     }
 
