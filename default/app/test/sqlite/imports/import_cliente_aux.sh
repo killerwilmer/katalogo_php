a8f5d94c12666cd9677bb2fd82f2e96b8fc73599
root <root@pruebas-quindesoft.(none)>
2014-04-20 17:48:31 +0000

    desde server

--- default/app/test/sqlite/imports/import_cliente_aux.sh
+++ default/app/test/sqlite/imports/import_cliente_aux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash"
 #killer
-sqlite3 /var/www/dbs/aux.db <<!
+sqlite3 /var/www/sqlite/dbs/aux.db <<!
 DELETE FROM "main"."cliente";
 .separator ,
 .import /var/www/katalogo_php/default/app/test/sqlite/csvs/clientes.csv cliente
