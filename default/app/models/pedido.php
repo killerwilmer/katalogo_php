fb75de3d295c267c942a99327a78d1647f1b630f
Cristhian Lombana <criferlo@gmail.com>
2014-04-20 18:09:42 +0100

    probando

---
+++ default/app/models/pedido.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of pedido
+ *
+ * @author usuario
+ */
+class Pedido extends ActiveRecord {
+    //put your code here
+     protected $database = "mysqlpedidos";
+}
+
+?>
